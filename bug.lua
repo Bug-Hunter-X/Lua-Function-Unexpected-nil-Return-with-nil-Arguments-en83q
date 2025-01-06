54f75273377c2dc265e3eab97543c227d48480f5
BugHunter-X <askzenai@gmail.com>
2025-01-06 23:28:04 +0100

    Add bug.lua file

---
+++ bug.lua
@@ -0,0 +1,13 @@
+local function foo(a, b)
+  if a == nil then
+    return b
+  elseif b == nil then
+    return a
+  else
+    return a + b
+  end
+end
+
+print(foo(nil, 10)) -- Output: 10
+print(foo(5, nil)) -- Output: 5
+print(foo(nil, nil)) -- Output: nil  -- Unexpected behavior if expecting 0
